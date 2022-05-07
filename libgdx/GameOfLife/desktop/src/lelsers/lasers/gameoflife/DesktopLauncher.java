ed18d13cdb06055b44aec44a42ac4b00164a71d5
Lord-Lelsers <48894200+LelsersLasers@users.noreply.github.com>
2022-05-06 18:38:51 -0700

    Default libgdx setup (java)

---
+++ libgdx/GameOfLife/desktop/src/lelsers/lasers/gameoflife/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package lelsers.lasers.gameoflife;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import lelsers.lasers.gameoflife.GameOfLife;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("GameOfLife");
+		new Lwjgl3Application(new GameOfLife(), config);
+	}
+}
