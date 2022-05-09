f1d0e3b0d13e184f16494d1b71f9a7d7a705aa17
Lord-Lelsers <millan.kumar.24@bishops.com>
2022-05-09 15:37:32 -0700

    Improving imports + updating JAR

--- libgdx/GameOfLife/core/src/lelsers/lasers/gameoflife/GameOfLife.java
+++ libgdx/GameOfLife/core/src/lelsers/lasers/gameoflife/GameOfLife.java
@@ -1,19 +1,11 @@
 package lelsers.lasers.gameoflife;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class GameOfLife extends Game {
 
--- libgdx/GameOfLife/desktop/src/lelsers/lasers/gameoflife/DesktopLauncher.java
+++ libgdx/GameOfLife/desktop/src/lelsers/lasers/gameoflife/DesktopLauncher.java
@@ -2,7 +2,6 @@ package lelsers.lasers.gameoflife;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import lelsers.lasers.gameoflife.GameOfLife;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
