5e10f3628f680bfa3e63e66d8dcd4e32e0b186de
Lord-Lelsers <millan.kumar.24@bishops.com>
2022-05-10 15:47:31 -0700

    Improving "settings" (final vars at top)

--- libgdx/GameOfLife/desktop/src/lelsers/lasers/gameoflife/DesktopLauncher.java
+++ libgdx/GameOfLife/desktop/src/lelsers/lasers/gameoflife/DesktopLauncher.java
@@ -6,17 +6,20 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 
+	// Settings
+	private static final int WIDTH = 45;
+	private static final int HEIGHT = 28;
 	private static final int SIZE = 16;
 	private static final int SPACER = 4;
-	private static final int FPS = 8;
+	private static final int INITIAL_FPS = 8;
 
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setResizable(false);
 		config.setWindowIcon("icon.png");
-		config.setWindowedMode(45 * (SIZE + SPACER) + SPACER, 28 * (SIZE + SPACER) + SPACER);
+		config.setWindowedMode(WIDTH * (SIZE + SPACER) + SPACER, HEIGHT * (SIZE + SPACER) + SPACER);
 
 		config.setTitle("Game Of Life");
-		new Lwjgl3Application(new GameOfLife(SIZE, SPACER, FPS), config);
+		new Lwjgl3Application(new GameOfLife(SIZE, SPACER, INITIAL_FPS), config);
 	}
 }
