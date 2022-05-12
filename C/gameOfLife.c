c75d14b4d8b5ec347c3097c0812ffbddac11258b
Lord-Lelsers <48894200+LelsersLasers@users.noreply.github.com>
2022-05-11 21:14:19 -0700

    Standardizing the C version
    width: 45
    height: 27
    spawnChance: 0.2
    delay: 50
    use x,y over i,j for 2d array

--- C/gameOfLife.c
+++ C/gameOfLife.c
@@ -3,10 +3,10 @@
 #include <time.h>
 #include <stdio.h>  
 
-#define width 45
-#define height 28
-#define aliveChanceOnSpawn .2
-#define delay 1
+#define WIDTH 45
+#define HEIGHT 27
+#define aliveChanceOnSpawn 0.2
+#define DELAY 50
 
 
 struct Cell {
@@ -14,38 +14,38 @@ struct Cell {
     int neighbors;
 };
 
-void draw(struct Cell cells[width][height]) {
+void draw(struct Cell cells[WIDTH][HEIGHT]) {
     system("CLS");
-    for (int i = 0; i < width + 2; i++) printf("#");
+    for (int i = 0; i < WIDTH + 2; i++) printf("#");
     printf("\n");
-    for (int yPos = 0; yPos < height; yPos++) {
+    for (int y = 0; y < HEIGHT; y++) {
         printf("#");
-        for (int xPos = 0; xPos < width; xPos++) {
-            if (cells[xPos][yPos].alive) printf(".");
+        for (int x = 0; x < WIDTH; x++) {
+            if (cells[x][y].alive) printf("@");
             else printf(" ");
         }
         printf("#\n");
     }
-    for (int i = 0; i < width + 2; i++) printf("#");
+    for (int i = 0; i < WIDTH + 2; i++) printf("#");
     printf("");
 }
 
-void updateNeighbors(struct Cell cells[width][height]) {
-    for (int i = 0; i < width; i++) {
-        for (int j = 0; j < height; j++) {
-            cells[i][j].neighbors = 0;
+void updateNeighbors(struct Cell cells[WIDTH][HEIGHT]) {
+    for (int x = 0; x < WIDTH; x++) {
+        for (int y = 0; y < HEIGHT; y++) {
+            cells[x][y].neighbors = 0;
             int offsets[8][2] = {{1, 0}, {1, 1}, {1, -1}, {0, 1}, {0, -1}, {-1, 0}, {-1, 1}, {-1, -1}};
-            for (int k = 0; k < 8; k++) {
-                if (i + offsets[k][0] >= 0 && i + offsets[k][0] < width && j + offsets[k][1] >= 0 && j + offsets[k][1] < height && cells[i + offsets[k][0]][j + offsets[k][1]].alive) {
-                    cells[i][j].neighbors++;
+            for (int i = 0; i < 8; i++) {
+                if (x + offsets[i][0] >= 0 && x + offsets[i][0] < WIDTH && y + offsets[i][1] >= 0 && y + offsets[i][1] < HEIGHT && cells[x + offsets[i][0]][y + offsets[i][1]].alive) {
+                    cells[x][y].neighbors++;
                 }
             }
         }
     }
-    for (int i = 0; i < width; i++) {
-        for (int j = 0; j < height; j++) {
-            if (cells[i][j].neighbors == 3) cells[i][j].alive = 1;
-            else if (cells[i][j].neighbors < 2 || cells[i][j].neighbors > 3) cells[i][j].alive = 0;
+    for (int x = 0; x < WIDTH; x++) {
+        for (int y = 0; y < HEIGHT; y++) {
+            if (cells[x][y].neighbors == 3) cells[x][y].alive = 1;
+            else if (cells[x][y].neighbors < 2 || cells[x][y].neighbors > 3) cells[x][y].alive = 0;
         }
     }
 }
@@ -53,17 +53,17 @@ void updateNeighbors(struct Cell cells[width][height]) {
 
 int main() {
     srand(time(NULL));
-    struct Cell cells[width][height];
-    for (int i = 0; i < width; i++) {
-        for (int j = 0; j < height; j++) {
+    struct Cell cells[WIDTH][HEIGHT];
+    for (int x = 0; x < WIDTH; x++) {
+        for (int y = 0; y < HEIGHT; y++) {
             int alive = ((double)rand()/(double)RAND_MAX < aliveChanceOnSpawn) ? 1 : 0; 
             struct Cell tempCell = {alive, 0};
-            cells[i][j] = tempCell;
+            cells[x][y] = tempCell;
         }
     }
     while (1) {
         draw(cells);
         updateNeighbors(cells);
-        _sleep(delay);
+        _sleep(DELAY);
     }
 }
